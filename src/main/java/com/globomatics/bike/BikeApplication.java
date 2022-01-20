9b3348de3bcd185b412de27d99e96492e3758b03
Juan Pablo Rivera riffo <juanp@Juans-MacBook-Pro.local>
2022-01-20 01:32:48 -0300

    first commit

---
+++ src/main/java/com/globomatics/bike/BikeApplication.java
@@ -0,0 +1,17 @@
+package com.globomatics.bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class BikeApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BikeApplication.class, args);
+    }
+}
