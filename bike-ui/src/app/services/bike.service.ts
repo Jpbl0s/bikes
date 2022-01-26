dc0a9ac1788647f52888ec49d824aa45fa617677
Juan Pablo Rivera riffo <juanp@Juans-MacBook-Pro.local>
2022-01-26 18:32:32 -0300

    adding get service with proxy to make calls to springboot app server

--- bike-ui/src/app/services/bike.service.ts
+++ bike-ui/src/app/services/bike.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { threadId } from 'worker_threads';
+
+const httpOptions = {
+  Headers : new HttpHeaders({'Content-Type':'application/json'})
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BikeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getBikes(){
+    return this.http.get('http://localhost:8080/server/api/v1/bikes');
+  }
 }
