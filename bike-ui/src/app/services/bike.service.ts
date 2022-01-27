973307a121b3a4d2fe2022914dcf4cad9a72d071
Juan Pablo Rivera riffo <juanp@Juans-MacBook-Pro.local>
2022-01-26 23:49:36 -0300

    last frontend changes

--- bike-ui/src/app/services/bike.service.ts
+++ bike-ui/src/app/services/bike.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { threadId } from 'worker_threads';
 
 const httpOptions = {
   Headers : new HttpHeaders({'Content-Type':'application/json'})
@@ -15,6 +13,6 @@ export class BikeService {
   constructor(private http:HttpClient) { }
 
   getBikes(){
-    return this.http.get('http://localhost:8080/server/api/v1/bikes');
+    return this.http.get('/server/api/v1/bikes');
   }
 }
